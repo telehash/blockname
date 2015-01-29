6c9c9f770550ad6c5d914366cc00774a9ba89ff7
Jeremie Miller <jeremie@jabber.org>
2015-01-28 20:57:54 -0700

    overhaul register tool, almost working on mainnet and better on testnet

--- bin/register.js
+++ bin/register.js
@@ -1,47 +1,77 @@
-var argv = require('yargs')
-  .describe('db','hint storage db directory')
-  .usage('Usage: $0 "domain" 1.2.3.4')
-  .demand(2)
-  .argv;
-
-var dbdir = argv.db || (__dirname + '/db');
-
 var bitcoin = require("bitcoinjs-lib");
 var opret = require('raw-op-return');
 var ip = require('ip');
 var level = require('level-party');
-var db = level(dbdir, { encoding: 'json' });
 
-var helloblock = require("helloblock-js")({
-  network: 'testnet'
-});
+var yargs = require('yargs')
+  .describe('db','hint storage db directory')
+  .describe('key','WIF format secret key to use for the registration transaction')
+  .boolean('test').describe('test','use a testnet faucet to fund the registration').default('test',true)
+  .usage('Usage: $0 "domain" 1.2.3.4 "txid" "refundto"')
+  .demand(2);
+var argv = yargs.argv;
 
 var domain = argv._[0];
-try {
-  var server = ip.toBuffer(argv._[1]);
-}catch(E){}
+try { var server = ip.toBuffer(argv._[1]); }catch(E){}
 if(!server || server.length != 4) return console.error("bad ip address",argv._[1]);
 
 var hint = "."+domain+server.toString("hex");
 if(hint.length > 40) return console.error("hint too large, domain must be <32 chars:",hint);
 
-console.log('registering',hint);
+var network = argv.test?"testnet":"mainnet";
+var key = argv.key ? bitcoin.ECKey.fromWIF(argv.key) : bitcoin.ECKey.makeRandom();
+var address = key.pub.getAddress(bitcoin.networks[network]).toString();
+var dbdir = argv.db || (__dirname + '/db');
+var db = level(dbdir, { encoding: 'json' });
+var helloblock = require("helloblock-js")({network:network});
 
-// generate a new temp wallet w/ some value
-helloblock.faucet.get(1, function(err, res, body) {
+console.log("using key '%s' (WIF)",key.toWIF(),argv.test);
+console.log("temporary address '%s'",address);
+console.log('registering hint "%s" to %s (OP_RETURN "%s")', domain, ip.toString(server), hint);
 
-  var privateKeyWIF = body.privateKeyWIF;
-  var address = body.address;
-  var unspentOutputs = body.unspents;
+// test mode, we use a faucet to get funds
+if(argv.test)
+{
+  helloblock.faucet.withdraw(address, 10000, function(err, res, ret) {
+    if(err) return console.error("faucet withdrawl failed",err);
+    helloblock.transactions.get(ret.txHash, function(err, res, tx) {
+      if(err) return console.error("fetching transaction failed",err);
+      register(key,address,tx,hint);
+    });
+  });
+  
+  return;
+}
 
-  var signFromPrivateKeyWIF = function(privateKeyWIF) {
-    return function(tx, callback) {
-      var key = bitcoin.ECKey.fromWIF(privateKeyWIF);
-      tx.sign(0, key); 
-      callback(false, tx);
-    }
+// live mode, require a source tx and refund address
+if(argv._.length != 4)
+{
+  yargs.showHelp()
+  process.exit(1);
+}
+  
+helloblock.transactions.get(argv._[2], function(err, res, tx) {
+  if(err) return console.error("fetching transaction failed",err);
+  register(key,argv._[3],tx,hint);
+});
+
+
+// actually do the registration transaction
+function register(from, to, source, hint){
+
+  // find the matching unspent output in the transaction
+  var unspent = false;
+  var match = from.pub.getAddress(bitcoin.networks[network]).toString();
+  source.outputs.forEach(function(output){
+    if(output.address == match) unspent = output;
+  });
+  if(!unspent) return console.error("no matching outputs found in transaction");
+  unspent.txHash = source.txHash;
+
+  var signTransaction = function(tx, callback) {
+    tx.sign(0, from); 
+    callback(false, tx);
   };
-  var signTransaction = signFromPrivateKeyWIF(privateKeyWIF);
   
   var propagateTransaction = function(tx, callback) {
     helloblock.transactions.propagate(tx, function(err, res, body) {
@@ -49,10 +79,11 @@ helloblock.faucet.get(1, function(err, res, body) {
     });
   };
   
+  // use the OP_RETURN module
   opret.post({
     stringData: hint,
-    address: address,
-    unspentOutputs: unspentOutputs,
+    address: to,
+    unspentOutputs: [unspent],
     propagateTransaction: propagateTransaction,
     signTransaction: signTransaction
   }, function(error, postedTx) {
@@ -63,5 +94,5 @@ helloblock.faucet.get(1, function(err, res, body) {
     });
   });
 
-});
+};
 
